196cd71b38ae020e0d17662e681072d81e210834
originalsamseal <90986476+originalsamseal@users.noreply.github.com>
2021-09-18 17:03:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* NICKNAMES */
 // List of nicknames
 const nicknames = {
     nickname1: '{ sammySeal }',
@@ -11,17 +12,40 @@ const nicknamesArray = Object.values(nicknames);
     console.log('Names to choose from: ' + nicknamesArray)
 
 // Nickname randomizer
-let i = Math.floor(Math.random() * nicknamesArray.length);
-const randomNickname = nicknamesArray[i];
+let n = Math.floor(Math.random() * nicknamesArray.length);
+const randomNickname = nicknamesArray[n];
         // CONSOLE TESTING
+    console.log('Nickname index: ' + n);
     console.log('Random nickname: ' + randomNickname);
 
-
-
-
-
+// DOM - Nickname manipulator
 let nicknameDisplay = document.getElementById('nickname');
 nicknameDisplay.innerHTML = randomNickname;
         // CONSOLE TESTING
     console.log('Name currently displayed: ' + nicknameDisplay.innerHTML);
+/* END NICKNAMES */
 
+/* PORTRAITS */
+// List of portraits
+const portraits = {
+    portrait1: 'assets/test1.jpg',
+    portrait2: 'assets/test2.jpg',
+    portrait3: 'assets/test3.jpg',
+    portrait4: 'assets/test4.png'
+}
+
+const portraitsArray = Object.values(portraits);
+        // CONSOLE TESTING
+    console.log('Portrait options: ' + portraitsArray);
+
+// Portrait randomizer
+let p = Math.floor(Math.random() * portraitsArray.length);
+const randomPortrait = portraitsArray[p];
+        // CONSOLE TESTING
+    console.log('Portrait index: ' + p);
+    console.log('Random portrait: ' + randomPortrait);
+
+let portraitDisplay = document.getElementById('sammy-portrait')
+portraitDisplay.src = randomPortrait;
+        // CONSOLE TESTING
+    console.log('Portrait currently displayed: ' + portraitDisplay.src);
